24bb29dc88c57d8df90f18ef2a14c343d61cc600
Ryan <rtheriot@hawaii.edu>
2018-11-19 16:20:07 -1000

    Added Plotly

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import style from "./main.css";
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import Plotly from 'plotly.js-dist';
 
 const arr = [1, 2, 3];
 const iAmJavascriptES6 = () => console.log(...arr);
@@ -12,3 +13,28 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(mymap);
+
+var trace1 = {
+	x: [1, 2, 3, 4],
+	y: [10, 15, 13, 17],
+	mode: 'markers',
+	type: 'scatter'
+  };
+  
+  var trace2 = {
+	x: [2, 3, 4, 5],
+	y: [16, 5, 11, 9],
+	mode: 'lines',
+	type: 'scatter'
+  };
+  
+  var trace3 = {
+	x: [1, 2, 3, 4],
+	y: [12, 9, 15, 12],
+	mode: 'lines+markers',
+	type: 'scatter'
+  };
+  
+  var data = [trace1, trace2, trace3];
+  
+  Plotly.newPlot('chart', data);
