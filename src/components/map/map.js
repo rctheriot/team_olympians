93864e2df411cc5c26b2b627950fa69406cda168
Ryan <rtheriot@hawaii.edu>
2018-11-22 16:54:32 -1000

    Added back query. Have to fix table. Started aspect ratio fix.

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -11,9 +11,11 @@ export class Map {
       Summer: [[0, 'rgb(253, 221, 225)'], [1.0, 'rgb(163, 10, 26)']],
       Winter: [[0, 'rgb(190,240,255)'], [1.0, 'rgb(10, 89, 128)']],
     }
-    this.projectionType = 'orthographic';
+    this.projectionType = 'robinson';
     this.mapData = [];
     this.season = 'Summer';
+    this.drawMap([], 'Summer');
+    this.plotlyMap = null;
   }
 
   drawMap(dataPoints, season) {
@@ -24,7 +26,7 @@ export class Map {
     this.season = season;
   
     this.mapData.forEach(el => {
-      locations.push(el['noc']);
+      locations.push(el['name']);
       z.push(el['total']);
       text.push(el['name']);
     });
@@ -73,16 +75,30 @@ export class Map {
       paper_bgcolor: "rgba(40,40,40, 1.0)",
     };
   
-    Plotly.newPlot(this.mapDiv, data, layout, { showLink: false, displayModeBar: true });
+    Plotly.newPlot(this.mapDiv, data, layout, { showLink: false, displayModeBar: false });
+    this.resize();
   }
 
   setLegendColor(season) {
-    document.getElementById('colorLegend').style = `background: linear-gradient(to right, ${colorScales[season][0][1]} 0%, ${colorScales[season][1][1]} 100%)`;
+    document.getElementById('colorLegend').style = `background: linear-gradient(to right, ${this.colorScales[season][0][1]} 0%, ${this.colorScales[season][1][1]} 100%)`;
   }
 
   setMapProjectionType(projectionType) {
     this.projectionType = projectionType;
     this.drawMap(this.mapData);
+  }
+
+  setSeason(season) {
+    this.season = season;
+    this.setLegendColor(season); 
+  }
+
+  resize() {
+    const w = this.mapDiv.parentNode.parentNode.parentNode.getBoundingClientRect().width;
+    const h = this.mapDiv.parentNode.parentNode.parentNode.getBoundingClientRect().height * 0.90;
+    const update = { width: w, height: h };
+ 
+    Plotly.relayout(this.mapDiv, update);
   }
 
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'purecss';
 
 // Import all classes
 import { Map } from './components/map/map.js';
-import { Database }from './components/database/database';
+import { Database } from './components/database/database';
 import { Table } from './components/table/table';
 
 // Map Setup
@@ -22,26 +22,59 @@ const medalTableDiv = document.getElementById('medalTableDiv')
 const medalTable = new Table(medalTableDiv);
 
 // Title and Query Setup
-const titleDiv = document.getElementById('title');
 let year = '2016';
-let sex = 'M';
+let sex = 'Male';
 let sport = 'Swimming';
-let season = 'Winter';
+let season = 'Summer';
+let query = {};
+setQuery();
+setTitle();
+const titleDiv = document.getElementById('title');
 
-function setTitle() {
-  titleDiv.innerText = `${year} ${season} Olympics - ${(sex == 'M') ? 'Male' : 'Female'} ${sport}`;
+function setQuery() {
+  query = { Year: { lte: year, gte: (Number(year) - 2).toString() }, Sport: sport, Season: season, Sex: sex };
+  if (sex == "Both") {
+    delete query['Sex'];
+    if (sport == 'All') {
+      delete query['Sport'];
+    }
+  }
+  console.log(query);
 }
 
-window.sportChange = (selSport) => {
-  sport = selSport;
-  setTitle();
-  query();
+function setTitle() {
+  if (titleDiv) {
+    titleDiv.innerText = `${year} ${season} Olympics - ${sex} ${sport}`;
+  }
 }
 
 window.yearChange = (selYear) => {
   year = selYear;
   setTitle();
-  query();
+  setQuery();
+  startQuery();
+}
+
+window.genderChange = (selGender) => {
+  sex = selGender;
+  setTitle();
+  setQuery();
+  startQuery();
+}
+
+window.sportChange = (selSport) => {
+  sport = selSport;
+  setTitle();
+  setQuery();
+  startQuery();
+}
+
+window.seasonChange = (selSeason) => {
+  season = selSeason;
+  setTitle();
+  setQuery();
+  plotlyMap.setSeason(season);
+  startQuery();
 }
 
 window.changeSortSelection = (selSort) => {
@@ -52,50 +85,48 @@ window.setMapProjectionType = (type) => {
   plotlyMap.setMapProjectionType(type);
 }
 
+window.onresize = () => {
+  plotlyMap.resize();
+}
 
-function query() {
+function startQuery() {
   const data = {};
-  const query = { Year: year };
-
   database.queryDatabase('athletes', query).forEach(el => {
     const country = el['NOC'];
     const event = el['Event'];
     const medal = el['Medal'];
 
     if (medal != 'NA') {
-      const countryInfo = database.queryDatabase('countryCodes', { NOC: country });
-      if (countryInfo) {
-        if (!data[countryInfo.Name]) {
-          data[countryInfo.Name] = {
-            'name': countryInfo.Name,
-            'noc': countryInfo.NOC,
-            'code': countryInfo.Code,
-            'gold': 0,
-            'silver': 0,
-            'bronze': 0,
-            'total': 0,
-            'events': []
-          }
+
+      if (!data[country]) {
+        data[country] = {
+          'name': country,
+          'gold': 0,
+          'silver': 0,
+          'bronze': 0,
+          'total': 0,
+          'events': []
         }
-        if (data[countryInfo.Name]['events'].indexOf(event) == -1) {
-          data[countryInfo.Name]['events'].push(event);
-          switch (medal) {
-            case 'Gold':
-              data[countryInfo.Name]['total'] += 1;
-              data[countryInfo.Name]['gold'] += 1;
-              break;
-            case 'Silver':
-              data[countryInfo.Name]['total'] += 1;
-              data[countryInfo.Name]['silver'] += 1;
-              break;
-            case 'Bronze':
-              data[countryInfo.Name]['total'] += 1;
-              data[countryInfo.Name]['bronze'] += 1;
-              break;
-          }
+      }
+      if (data[country]['events'].indexOf(event) == -1) {
+        data[country]['events'].push(event);
+        switch (medal) {
+          case 'Gold':
+            data[country]['total'] += 1;
+            data[country]['gold'] += 1;
+            break;
+          case 'Silver':
+            data[country]['total'] += 1;
+            data[country]['silver'] += 1;
+            break;
+          case 'Bronze':
+            data[country]['total'] += 1;
+            data[country]['bronze'] += 1;
+            break;
         }
       }
     }
+
   });
 
   const dataArray = [];
@@ -103,11 +134,10 @@ function query() {
   Object.keys(data).forEach(country => {
     delete data[country]['events'];
     dataArray.push(data[country]);
-    delete data[country]['noc'];
     tableArray.push(data[country])
   });
 
-  medalTable.updateTableData(tableArray);
+  // medalTable.updateTableData(tableArray);
 
   plotlyMap.drawMap(dataArray, season);
 }
